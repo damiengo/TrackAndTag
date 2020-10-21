484809734b0a5be7cc95be14a2c5891264b1d8bd
damiengo <d@geon.fr>
2020-10-21 22:35:09 +0200

    Activities test

--- __tests__/Activities-test.js
+++ __tests__/Activities-test.js
@@ -5,7 +5,7 @@ import ActivitiesScreen from '../src/screens/Activities';
 import ActionButton from 'react-native-action-button';
 
 import { configure, shallow } from 'enzyme';
-
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -20,3 +20,25 @@ it('add an item', () => {
   wrapper.find(ActionButton).simulate('press')
   expect(navigation.navigate.mock.calls.length).toEqual(1);
 });
+
+it('list existing items', () => {
+    const activity = {
+        tags: 'tag1 tag2'.split(' '), 
+        title: 'The title', 
+        description: 'A short description', 
+        number: 18, 
+        date: new Date(), 
+        createdAt: new Date(), 
+        updatedAt: new Date()
+    }
+    var a = {}
+    const key = activity.createdAt.getTime()
+    a[key] = activity
+    mockAsyncStorage.setItem('@activities', JSON.stringify(a))
+    const wrapper = shallow(<ActivitiesScreen />)
+    const flData = wrapper
+             .find('FlatList')
+             .props()
+             .data
+    //expect(flData).toHaveLength(1)
+})
